679429d1e8d3f6c124fe52d7df35beedf214f6c6
pduch99 <42098547+pduch99@users.noreply.github.com>
2019-11-04 13:15:13 -0600

    Update Credit.java
    added credit card validator

--- Restaurant/src/Boba/Credit.java
+++ Restaurant/src/Boba/Credit.java
@@ -1,5 +1,127 @@
 package Boba;
-
+import java.util.Scanner;
 public class Credit {
-
+	
+	private String cardName;
+	private long cardNumber;
+	private int cardMonth;
+	private int cardYear;
+	private int cardSSV;
+	
+	public void setCardName(String cardName) {
+		this.cardName = cardName;
+	}
+	public String getCardName() {
+		return cardName;
+	}
+	public void setCardMonth(int cardMonth) {
+		this.cardMonth = cardMonth;
+	}
+	public int getCardMonth() {
+		return cardMonth;
+	}
+	public void setCardYear(int cardYear) {
+		this.cardYear = cardYear;
+	}
+	public int getCardYear() {
+		return cardYear;
+	}
+	public void setCardSSV(int cardSSV) {
+		this.cardSSV = cardSSV;
+	}
+	public int getCardSSV() {
+		return cardSSV;
+	}
+	
+	
+	public void setCardNumber(long cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+	public long getCardNumber() {
+		return cardNumber;
+	}
+	public static void main(String[] args) 
+    { 
+		Scanner input = new Scanner(System.in); 
+        Credit c1 = new Credit();
+        System.out.println("Card Name:");
+        c1.setCardName(input.nextLine());
+        System.out.println("Card Number:");
+        c1.setCardNumber(input.nextLong());
+        System.out.println("Card Month:");
+        c1.setCardMonth(input.nextInt());
+        System.out.println("Card Year:");
+        c1.setCardYear(input.nextInt());
+        System.out.println("Card SSV:");
+        c1.setCardSSV(input.nextInt());
+        long number = c1.getCardNumber();
+        
+        System.out.println(number + " is " +  
+        (isValid(number) ? "valid" : "invalid")); 
+    } 
+  
+    // Return true if the card number is valid 
+    public static boolean isValid(long number) 
+    { 
+       return (getSize(number) >= 13 &&  
+               getSize(number) <= 16) &&  
+               (prefixMatched(number, 4) ||  
+                prefixMatched(number, 5) ||  
+                prefixMatched(number, 37) ||  
+                prefixMatched(number, 6)) &&  
+              ((sumOfDoubleEvenPlace(number) +  
+                sumOfOddPlace(number)) % 10 == 0); 
+    } 
+  
+    // Get the result from Step 2 
+    public static int sumOfDoubleEvenPlace(long number) 
+    { 
+        int sum = 0; 
+        String num = number + ""; 
+        for (int i = getSize(number) - 2; i >= 0; i -= 2)  
+            sum += getDigit(Integer.parseInt(num.charAt(i) + "") * 2); 
+          
+        return sum; 
+    } 
+  
+    // Return this number if it is a single digit, otherwise, 
+    // return the sum of the two digits 
+    public static int getDigit(int number) 
+    { 
+        if (number < 9) 
+            return number; 
+        return number / 10 + number % 10; 
+    } 
+  
+    // Return sum of odd-place digits in number 
+    public static int sumOfOddPlace(long number) 
+    { 
+        int sum = 0; 
+        String num = number + ""; 
+        for (int i = getSize(number) - 1; i >= 0; i -= 2)  
+            sum += Integer.parseInt(num.charAt(i) + "");         
+        return sum; 
+    } 
+  
+    // Return true if the digit d is a prefix for number 
+    public static boolean prefixMatched(long number, int d) 
+    { 
+        return getPrefix(number, getSize(d)) == d; 
+    } 
+  
+    // Return the number of digits in d 
+    public static int getSize(long d) 
+    { 
+        String num = d + ""; 
+        return num.length(); 
+    } 
+    public static long getPrefix(long number, int k) 
+    { 
+        if (getSize(number) > k) { 
+            String num = number + ""; 
+            return Long.parseLong(num.substring(0, k)); 
+        } 
+        return number; 
+    } 
+	
 }
