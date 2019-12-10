e71b9bfbccd61359c4eb5b734407a52cd8b61c2e
rs6787ys <rs6787ys@W3165-S1904275.century.edu>
2019-12-10 13:54:42 -0600

    Added the employee class

---
+++ Final/src/Source/Employee.java
@@ -0,0 +1,35 @@
+package Source;
+
+public class Employee 
+{
+	private String name;  //The name of the employee
+	private String passwd; //The password for the employee
+	
+	public Employee(String empName, String empPassword)
+	{
+		this.name = empName;
+		this.passwd = empPassword;
+	}
+	
+	//Returns true if the name of the employee object that calls this is the same as the name given
+	public boolean isName(String req)
+	{
+		boolean result = false;
+		
+		if(req.equalsIgnoreCase(this.name))
+		{ result = true; }
+		
+		return result;
+	}
+	
+	//Returns true if the password of the calling employee is the same
+	public boolean isPassword(String req)
+	{
+		boolean result = false;
+		
+		if(req.equals(this.passwd))
+		{ result = true; }
+		
+		return result;
+	}
+}
